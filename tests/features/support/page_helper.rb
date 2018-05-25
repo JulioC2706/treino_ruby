23fb02f99eec7cc42e388c4da4c681a859325e04
Julio Cesar <jcsilva@svlabs.com.br>
2018-05-25 14:27:11 -0300

    Treino Ruby

---
+++ tests/features/support/page_helper.rb
@@ -0,0 +1,12 @@
+Dir[File.join(File.dirname(__FILE__), 
+                            '../pages/*_page.rb')].each { |file| require file }
+
+module Pages
+    def home
+        @home ||= HomePage.new
+    end
+
+    def pagina_professor
+        @pagina_professor ||= PaginaProfessor.new
+    end
+end
